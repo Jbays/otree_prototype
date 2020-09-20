84f8b149d97e695161897ee80fb6c94a3aa89e49
jbays <jjbaize@gmail.com>
2020-09-19 21:21:51 -0500

    Removing unnecessary code.  Also have the quiz logic working.  Now duplicate the main part of the experiment.  Then pass session data between the duplicated parts.  Aaaand I think it's working.

--- intro_and_quiz/models.py
+++ intro_and_quiz/models.py
@@ -8,63 +8,40 @@ from otree.api import (
     Currency,
 )
 
-
 doc = """
 Here is my first oTree experimental economics program.
 """
-
 
 class Constants(BaseConstants):
     print('creating the constants class')
     num_rounds = 1
     players_per_group = None
     name_in_url = 'intro_and_quiz'
-    # instructions_template = 'dynamic_optimization_experiment/instructions.html'
-    # decision_box_component = 'dynamic_optimization_experiment/DecisionBox.html'
-    # calculator_component = 'dynamic_optimization_experiment/CalculatorComponent.html'
-    # output_to_points_converter_component = 'dynamic_optimization_experiment/OutputToPointsConverter.html'
-    # output_to_points_conversion_component = 'dynamic_optimization_experiment/OutputToPointsConversion.html'
 
 class Subsession(BaseSubsession):
     pass
 
-
 class Group(BaseGroup):
     print('creating the Group class')
-    # two_thirds_avg = models.FloatField()
-    # best_guess = models.IntegerField()
-    # num_winners = models.IntegerField()
-
-    # def set_payoffs(self):
-    #     print('initiating set_payoffs')
-        # players = self.get_players()
-        # guesses = [p.guess for p in players]
-        # two_thirds_avg = (2 / 3) * sum(guesses) / len(players)
-        # self.two_thirds_avg = round(two_thirds_avg, 2)
-
-        # self.best_guess = min(
-        #     guesses, key=lambda guess: abs(guess - self.two_thirds_avg)
-        # )
-
-        # winners = [p for p in players if p.guess == self.best_guess]
-        # self.num_winners = len(winners)
-
-        # for p in winners:
-        #     p.is_winner = True
-        #     p.payoff = Constants.jackpot / self.num_winners
-
-    # def two_thirds_avg_history(self):
-    #     print('initiating two_thirds_avg_history')
-        # return [g.two_thirds_avg for g in self.in_previous_rounds()]
-
 
 class Player(BasePlayer):
     print('creating the Player class')
-    # token_balance = 0
-    # token_balance = models.IntegerField(label="Token Balance:",initial=0)
-    # purchased_units = 0
-    
     quiz_question_1 = models.StringField(
         choices=['a','b','c']
     )
+
+    def quiz_question_1_error_message(self,value):
+        print('value is:',value)
+        if ( value != 'c' ):
+            return 'You do not understand the instructions of this game.'
+
+    quiz_question_2 = models.StringField(
+        choices=['true','false'],
+        widget=widgets.RadioSelect
+    )
+
+    def quiz_question_2_error_message(self,value):
+        print('value is:',value)
+        if ( value != 'true' ):
+            return 'You. Are. Irredeemable.'
     
