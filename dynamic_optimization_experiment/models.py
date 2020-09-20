abbcf0029a1daa6d62c3b559d3eecf0e3dd3d121
jbays <jjbaize@gmail.com>
2020-09-19 22:48:53 -0500

    Created a random string generator.  Will use this to dictate the flow of the experiments.  Now just need to pass this data to the front-end -- to be processed by the calculator.

--- dynamic_optimization_experiment/models.py
+++ dynamic_optimization_experiment/models.py
@@ -18,9 +18,6 @@ class Constants(BaseConstants):
     print('creating the constants class')
     num_rounds = 18
     name_in_url = 'dyn_opt_exp'
-    income = 1000
-    price_per_unit = 100
-    interest_rate = 10
     instructions_template = 'dynamic_optimization_experiment/instructions.html'
     decision_box_component = 'dynamic_optimization_experiment/DecisionBox.html'
     calculator_component = 'dynamic_optimization_experiment/CalculatorComponent.html'
@@ -32,58 +29,66 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     pass
 
-
 class Group(BaseGroup):
     print('creating the Group class')
-    # two_thirds_avg = models.FloatField()
-    # best_guess = models.IntegerField()
-    # num_winners = models.IntegerField()
 
-    # def set_payoffs(self):
-    #     print('initiating set_payoffs')
-        # players = self.get_players()
-        # guesses = [p.guess for p in players]
-        # two_thirds_avg = (2 / 3) * sum(guesses) / len(players)
-        # self.two_thirds_avg = round(two_thirds_avg, 2)
+def randomNumbersStringified():
+    import random
 
-        # self.best_guess = min(
-        #     guesses, key=lambda guess: abs(guess - self.two_thirds_avg)
-        # )
+    arr = []
+    output = ''
 
-        # winners = [p for p in players if p.guess == self.best_guess]
-        # self.num_winners = len(winners)
+    for num in range(1,10):
+        arr.append(str(num))
+        # output += num
 
-        # for p in winners:
-        #     p.is_winner = True
-        #     p.payoff = Constants.jackpot / self.num_winners
+    random.shuffle(arr)
 
-    # def two_thirds_avg_history(self):
-    #     print('initiating two_thirds_avg_history')
-        # return [g.two_thirds_avg for g in self.in_previous_rounds()]
+    for num in range(0,9):
+        output += arr[num]
 
+    return output
+
+randomStr = randomNumbersStringified()
 
 class Player(BasePlayer):
     print('creating the Player class')
+    purchased_units = models.FloatField(label="Purchased Units:",min=0)
+    
+    print('this randomStr is our final answer:',randomStr)
+
+    experiment_sequence = models.StringField(initial=randomStr)
+
+    # inflation = models.FloatField()
+
+    # def inflation_choices(self):
+    #     # import random
+    #     # choices = []
+    #     print('hello from inflation_choices')
+    #     print('self',self)
+    #     print('self.session.vars',self.session.vars)
+    #     # if 
+
+    
     # token_balance = 0
     # token_balance = models.IntegerField(label="Token Balance:",initial=0)
     # purchased_units = 0
-    purchased_units = models.FloatField(label="Purchased Units:",min=0)
-    final_tokens_balance = models.FloatField(label="Final Tokens Balance",initial=0)
+    # final_tokens_balance = models.FloatField(label="Final Tokens Balance",initial=0)
     
-    def final_tokens_balance_error_message(self,final_token_balance):
-        print('final_tokens_balance_error_message')
-        print('final token balance is',final_token_balance)
-        if ( final_token_balance < self.debt_limit ):
-            print('your debt level should be checked!')
+    # def final_tokens_balance_error_message(self,final_token_balance):
+    #     print('final_tokens_balance_error_message')
+    #     print('final token balance is',final_token_balance)
+    #     if ( final_token_balance < self.debt_limit ):
+    #         print('your debt level should be checked!')
 
+    # # debt_limit = models.CurrencyField()
     # debt_limit = models.CurrencyField()
-    debt_limit = models.CurrencyField()
 
-    def debt_limit_max(self):
-        print('calculate debt_limit_max')
-        print('self',self)
-        return 0
+    # def debt_limit_max(self):
+    #     print('calculate debt_limit_max')
+    #     print('self',self)
+    #     return 0
 
-    def example(self):
-        print('random_name self',self)
+    # def example(self):
+    #     print('random_name self',self)
     
