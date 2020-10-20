b1d15a315f9cbec1d6d2a80d9484fa796a68604e
jbays <jjbaize@gmail.com>
2020-10-19 22:24:09 -0500

    Obscure a given column is working.  Fixed a small bug too.

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ SESSION_CONFIG_DEFAULTS = dict(
     start_token_balance = 900,
     final_token_balance = 900,
     # comma-separated --> name of column, then first number is the start period, second number is the stop period
-    obscure_this_column_name_at_certain_period = "inflation,5,10",
+    obscure_this_column_name_at_certain_period = "total_points,5,7",
     # hello_number=0,
     # hello_boolean=True,
     # hello_string="string",
